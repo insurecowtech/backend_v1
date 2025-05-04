098ba6766680fa800e7ba10c45419b2c905fd39d
shafiqulislamshawon <shafiqshawonofficial@gmail.com>
2025-05-04 12:44:18 +0600

    WIP

--- Insurecow/urls.py
+++ Insurecow/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 
+
 ]

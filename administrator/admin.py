46580ef58edb450612a412494846359cacd6226b
shafiqulislamshawon <shafiqshawonofficial@gmail.com>
2025-05-08 18:45:57 +0600

    WIP

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from administrator.models import AuditLog
+
 # Register your models here.
+admin.site.register(AuditLog)
--- assetservice/admin.py
+++ assetservice/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from assetservice.models import AssetType, Asset
+from assetservice.models import *
 
 # Register your models here.
 admin.site.register(AssetType)
 admin.site.register(Asset)
+admin.site.register(Breed)
+admin.site.register(Color)
+admin.site.register(VaccinationStatus)
+admin.site.register(DewormingStatus)
+admin.site.register(AssetHistory)
